e95522c87aaf67e99c731ded0bd38892e1427b35
Andrey Malakhov <a.malakhov@mobidev.biz>
2014-03-06 18:14:13 +0200

    Init

---
+++ connectlib/src/main/java/blak/android/connectlib/MainActivity.java
@@ -0,0 +1,11 @@
+package blak.android.connectlib;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
