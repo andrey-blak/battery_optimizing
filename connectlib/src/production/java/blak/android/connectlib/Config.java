878f741210a6621eeab76660658f251be1aeff0b
Andrey Malakhov <a.malakhov@mobidev.biz>
2014-03-07 16:36:18 +0200

    Add build flavors

---
+++ connectlib/src/production/java/blak/android/connectlib/Config.java
@@ -0,0 +1,5 @@
+package blak.android.connectlib;
+
+public class Config {
+    public static String BASE_URL = "http://production.blabla.bla";
+}
---
+++ connectlib/src/stage/java/blak/android/connectlib/Config.java
@@ -0,0 +1,5 @@
+package blak.android.connectlib;
+
+public class Config {
+    public static String BASE_URL = "http://staging.blabla.bla";
+}
